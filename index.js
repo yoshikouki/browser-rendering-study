b80c99837a1d54f8558ef4da63edc6bb98edb439
yoshikouki <yoshikouki@gmail.com>
2024-04-18 01:12:11 +0900

    Output to CSV format

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { readdirSync } from 'node:fs';
+import fs from 'node:fs';
 import { join, dirname } from 'node:path';
 import { chromium } from 'playwright';
 import { fileURLToPath } from 'node:url';
@@ -6,12 +6,12 @@ import { fileURLToPath } from 'node:url';
 const fileName = fileURLToPath(import.meta.url);
 const dirName = dirname(fileName);
 const testsDir = join(dirName, 'tests');
-const testFiles = readdirSync(testsDir).filter(file => file.endsWith('.html'));
+const testFiles = fs.readdirSync(testsDir).filter(file => file.endsWith('.html'));
+
+const results = [];
 
 const browser = await chromium.launch();
 const page = await browser.newPage();
-
-const results = [];
 for (const file of testFiles) {
     const filePath = `file://${join(testsDir, file)}`;
     await page.goto(filePath);
@@ -20,7 +20,17 @@ for (const file of testFiles) {
     });
     results.push(...result);
 }
-
-console.log(results);
-
 await browser.close();
+
+const now = new Date();
+const dateString = now.toISOString().slice(0, 19).replace(/:/g, '-');
+const csvFilePath = join(dirName, 'out', `${dateString}.csv`);
+
+const header = `${Object.keys(results[0]).join(',')}\n`;
+fs.writeFileSync(csvFilePath, header);
+for (const result of results) {
+    const values = `${Object.values(result).join(',')}\n`;
+    fs.appendFileSync(csvFilePath, values);
+}
+
+console.log(`Completed: ${csvFilePath}`);
